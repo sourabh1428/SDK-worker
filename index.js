49d398968be9203e61891ef6a5fc2af451eaebad
Sourabh <sppathak1428@gmail.com>
2024-08-25 20:20:08 +0530

    :Da

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const hue = require('./UISrefresh');
 const transferData = require('./redisToMongo');
 
-function refreshInterval() {
-    // Define the minimum and maximum delay in milliseconds (0 to 60,000 ms for up to 1 minute)
-    const minDelay = 10000; // Minimum delay (e.g., immediately)
-    const maxDelay = 60000; // Maximum delay (e.g., 1 minute)
+// function refreshInterval() {
+//     // Define the minimum and maximum delay in milliseconds (0 to 60,000 ms for up to 1 minute)
+//     const minDelay = 10000; // Minimum delay (e.g., immediately)
+//     const maxDelay = 60000; // Maximum delay (e.g., 1 minute)
 
-    // Generate a random delay between minDelay and maxDelay
-    const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+//     // Generate a random delay between minDelay and maxDelay
+//     const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
 
-    // Execute the transferData and hue functions
-    console.log("Transfer from redis to mongo done and UISrefresh");
+//     // Execute the transferData and hue functions
+//     console.log("Transfer from redis to mongo done and UISrefresh");
 
-    transferData();
-    hue();
+//     transferData();
+//     hue();
 
-    // Schedule the next refresh
-    setTimeout(refreshInterval, randomDelay);
-}
+//     // Schedule the next refresh
+//     setTimeout(refreshInterval, randomDelay);
+// }
 
-// Start the refresh cycle
-refreshInterval();
+// // Start the refresh cycle
+// refreshInterval();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Keep-alive route
+hue();
+
+transferData();
+
+
+app.get('/keep-alive', (req, res) => {
+  res.send('I am alive - from worker');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
